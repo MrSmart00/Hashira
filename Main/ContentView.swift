8c5413b1ecc0c77ce4339580ab61d6ab002cc4a6
hiroya-hinomori <hiroya@hiroya-hinomorinoMacBook-Pro.local>
2021-06-13 00:10:30 +0900

    Fix: rename default target name

---
+++ Main/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Main
+//
+//  Created by hiroya-hinomori on 2021/06/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Main/MainApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainApp.swift
+//  Main
+//
+//  Created by hiroya-hinomori on 2021/06/13.
+//
+
+import SwiftUI
+
+@main
+struct MainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
