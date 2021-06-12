3f1c02380b1643d2b52d6f40629b472f81abd11b
hiroya-hinomori <hiroya@hiroya-hinomorinoMacBook-Pro.local>
2021-06-13 00:30:35 +0900

    Fix: Main target divided

---
+++ Content/Content.h
@@ -0,0 +1,18 @@
+//
+//  Content.h
+//  Content
+//
+//  Created by hiroya-hinomori on 2021/06/13.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Content.
+FOUNDATION_EXPORT double ContentVersionNumber;
+
+//! Project version string for Content.
+FOUNDATION_EXPORT const unsigned char ContentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Content/PublicHeader.h>
+
+
---
+++ Content/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Main
+//
+//  Created by hiroya-hinomori on 2021/06/13.
+//
+
+import SwiftUI
+
+public struct ContentView: View {
+    public init() { }
+    
+    public var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
