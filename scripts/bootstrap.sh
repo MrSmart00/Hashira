780bc4d1930c3c544b16aad21044880d9767da63
hiroya-hinomori <hiroya@hiroya-hinomorinoMacBook-Pro.local>
2021-06-13 14:29:44 +0900

    Add: static framework setting on project.yml

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -5,3 +5,8 @@ mint run xcodegen generate
 # bundle exec pod install --repo-update
 
 echo "🏁 \033[32mBootstrap Completed!\033[m 🏁"
+
+ROOT=$(git rev-parse --show-toplevel)
+PROJECT_NAME=$(basename ${ROOT})
+
+open $PROJECT_NAME.xcodeproj
